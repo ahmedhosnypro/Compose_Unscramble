ef087f5089fd271a7a2be3674659e44f250d5850
Ahmed Hosny <ahhosnyas@gmail.com>
2023-08-07 05:55:19 +0300

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+j
+:app/src/main/java/com/example/unscramble/data/WordsData.kt,3\7\37a30ae319a28120229f8f4aa394151a8bf70c04
+j
+:app/src/main/java/com/example/unscramble/ui/theme/Color.kt,6\f\6f5da1de86e8917fc3d4f42376a0d7bd36d743e9
+j
+:app/src/main/java/com/example/unscramble/ui/theme/Theme.kt,0\1\018cc0f21e8823ee2ba9857a694922c649b801eb
+i
+9app/src/main/java/com/example/unscramble/ui/theme/Type.kt,6\d\6d5be3ace9c56635847a10e9475e4dbe6b78b76e
+@
+build.gradle.kts,d\b\dbcff70658daf80b53ce624f6adcaa529df5ed8d
+D
+app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+l
+<app/src/main/java/com/example/unscramble/ui/GameViewModel.kt,6\d\6d3c2ed34eed6f071d817c5d62d80ad904e2a140
---
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.unscramble.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.allWords
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class GameViewModel : ViewModel() {
+    private var usedWords: MutableSet<String> = mutableSetOf()
+    val currentScrambledWord: String = ""
+    private lateinit var currentWord: String
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        resetGame()
+    }
+
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+
+    private fun pickRandomWordAndShuffle(): String {
+        // Continue picking up a new random word until you get one that hasn't been used before
+        currentWord = allWords.random()
+        return if (usedWords.contains(currentWord)) {
+            pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            shuffleCurrentWord(currentWord)
+        }
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        // Scramble the word
+        tempWord.shuffle()
+        while (String(tempWord) == word) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+}
